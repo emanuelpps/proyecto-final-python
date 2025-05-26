d7221c8a4c37f846f3f9d02ad9a78cd95731fa8d
emanuelpps <emanuelpages.ps@gmail.com>
2025-05-26 13:51:26 -0300

    fix: added observer pattern

--- logger.py
+++ logger.py
@@ -2,52 +2,61 @@ from functools import wraps
 import requests
 import datetime
 
+# Patrón Observador
+
+
+class Observer:
+    def update(self, message):
+        raise NotImplementedError(
+            "El observer debe implementar el método update()")
+
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def attach(self, observer: Observer):
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer):
+        self._observers.remove(observer)
+
+    def notify(self, message):
+        for observer in self._observers:
+            observer.update(message)
+
 # Observers
 
 
-class ConsoleLogger:
+class ConsoleLogger(Observer):
     def update(self, message):
         print(f"[CONSOLE LOG] {datetime.datetime.now()}: {message}")
 
 
-class FileLogger:
+class FileLogger(Observer):
     def update(self, message):
         with open("logs_observer.txt", "a", encoding="utf-8") as f:
             f.write(f"[FILE LOG] {datetime.datetime.now()}: {message}\n")
 
 
-class ServerLogger:
+class ServerLogger(Observer):
     def update(self, message):
         try:
             requests.post("http://localhost:9090", json={"message": message})
         except Exception as e:
             print(f"[SERVER LOG ERROR] {e}")
 
-# observador central
+# Conf del Subject
 
 
-class LoggerSubject:
-    def __init__(self):
-        self._observers = []
-
-    def attach(self, observer):
-        self._observers.append(observer)
-
-    def notify(self, message):
-        for observer in self._observers:
-            observer.update(message)
-
-
-# Conf del logger
-logger_subject = LoggerSubject()
+logger_subject = Subject()
 logger_subject.attach(ConsoleLogger())
 logger_subject.attach(FileLogger())
 logger_subject.attach(ServerLogger())
 
+
 # Decorador
-
-
-def log_action(logger):
+def log_action(logger: Subject):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
